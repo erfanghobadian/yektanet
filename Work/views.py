2d772a61f16a439dfaed0b54656d7c29dfa82c48
Erfan Ghobadian <de.rfan@icloud.com>
2020-08-24 17:36:03 +0430

    Add Salary Range Search

--- Work/views.py
+++ Work/views.py
@@ -9,14 +9,75 @@ from User import permissions as user_permissions
 from rest_framework.response import Response
 from rest_framework import status
 from django.utils import timezone
-from datetime import timedelta
+from rest_framework import filters
+
+
+class Node:
+    def __init__(self, key, pk):
+        self.left = None
+        self.right = None
+        self.val = key
+        self.pk = pk
+
+
+def sortedArrayToBST(arr):
+    if not arr:
+        return None
+
+    mid = int((len(arr)) / 2)
+
+    root = arr[mid]
+
+    root.left = sortedArrayToBST(arr[:mid])
+    root.right = sortedArrayToBST(arr[mid + 1:])
+    return root
+
+
+def RangeFilter(mn, mx, root):
+    ans = []
+
+    def rec(minr, maxr, rootr):
+        if rootr is None:
+            return
+        if rootr.val < minr:
+            rec(minr, maxr, rootr.right)
+        if rootr.val > maxr:
+            rec(minr, maxr, rootr.left)
+        if minr <= rootr.val <= maxr:
+            ans.append(rootr.pk)
+            rec(minr, maxr, rootr.left)
+            rec(minr, maxr, rootr.right)
+        return
+
+    rec(mn, mx, root)
+    return ans
+
+
+class SalaryRangeFilter(filters.BaseFilterBackend):
+    def filter_queryset(self, request, queryset, view):
+        min_salary = request.query_params.get("min_salary", 0)
+        max_salary = request.query_params.get("max_salary", 1000000000000000000)
+        works = Work.objects.all()
+        arr = []
+        for work in works:
+            node = Node(work.salary, work.pk)
+            arr.append(node)
+        arr = sorted(arr, key=lambda nd: nd.val)
+        root = sortedArrayToBST(arr)
+        pks = RangeFilter(int(min_salary), int(max_salary), root)
+        qs = Work.objects.filter(pk__in=pks)
+        return qs
 
 
 class WorkList(generics.ListAPIView):
     def get_queryset(self):
         return Work.objects.all()
+
     serializer_class = WorkSerializer
     permission_classes = [IsAuthenticated]
+    filter_backends = [filters.OrderingFilter, filters.SearchFilter, SalaryRangeFilter]
+    ordering_fields = ['expire_date', 'hours', 'salary']
+    search_fields = ['title']
 
 
 class WorkDetail(generics.RetrieveAPIView):
@@ -58,7 +119,7 @@ class SubmitWork(generics.CreateAPIView):
 
     def post(self, request, *args, **kwargs):
         work = Work.objects.get(pk=request.data['work'])
-        if timezone.now() < work.expire_date :
+        if timezone.now() < work.expire_date:
             submit, created = WorkSubmit.objects.get_or_create(person=request.user.personprofile, work=work)
             submit.save()
             return Response("Your Application Added Successfully", status=status.HTTP_200_OK)
